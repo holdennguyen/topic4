92566bf1224feaa1c2a8c6529f1e0f52917274cb
holdennguyen <holdennguyen6174@gmail.com>
2023-02-25 10:18:50 +0700

    first commit

---
+++ InstallAnsibleController.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install the ansible
+amazon-linux-extras install epel -y
+amazon-linux-extras install ansible2 -y
+
+# Update server packages
+yum update -y
+
+# Add user ansible admin
+useradd ansibleadmin
+
+# Set password: the below command will avoid re-entering the password
+echo "ansibleadmin" | passwd --stdin ansibleadmin
+
+# Modify the sudoers file at /etc/sudoers and add entry
+echo 'ansibleadmin  ALL=(ALL)   NOPASSWD: ALL' | tee -a /etc/sudoers
+echo 'ec2-user ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers
+
+# Enable Password Authentication
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
